95b644f652efe4ef0b8362eca4b91f988710332e
Viktor Bogutskii <vbogutskii@equinix.com>
2020-06-27 10:31:58 -0700

    initial redux setup: store, reducer

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+
+// Reducer todo
+const initialState = {};
+const todo = (state = initialState, action) => {
+};
+
+
+const store = createStore(
+  todo
+);
+
+export default store;
