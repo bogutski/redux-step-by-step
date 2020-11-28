0a14534dde71debd64c40cee2a09771be04cb976
Viktor Bogutskii <info@siteograf.com>
2020-11-27 18:33:36 -0800

    upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import todo from './reducer';
 
-const store = createStore(todo, composeWithDevTools(
-  applyMiddleware(),
-));
+const store = createStore(
+  todo,
+  composeWithDevTools(applyMiddleware())
+);
 
 export default store;
